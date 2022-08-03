6f78c2b17b9fb31a2aa318fef7ad69928e7472cd
Daria <d.orlik000@gmail.com>
2022-08-03 17:43:49 +0300

    /

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        ;
+        int clientOs = 0;
+        if (clientOs == 0) {
+            System.out.println("У вас андроид");
+        } else if (clientOs == 1) {
+            System.out.println("У вас ОС");
+        } else {
+            System.out.println("ОС не найдена");
+
+            int year = 205;
+            if (clientOs == 0 && year > 2019) {
+                System.out.println("установите обычную версию андроида");
+            } else if ()
+            }
+
+        }
+
     }
+
 }
